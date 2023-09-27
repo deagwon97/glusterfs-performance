4b7192081dcbc9ce1465372c36645c227bcdec06
deagwon97 <curiosity806@naver.com>
2023-09-27 15:28:23 +0900

    update

--- multiple-file-io-test.sh
+++ multiple-file-io-test.sh
@@ -2,36 +2,39 @@
 
 
 test_io() {
-    local vol=$1            # the path of test workspace in the volume
-    local bs=$2             # block size nk, nM, nG ..
-    local parallel_count=$3 # the number of parallel processes
-    local copy_count=$4     # the number of copy input blocks
 
-    # create io test workspace
+    local vol=$1
+    local bs=$2
+    local separately_count=$3
+
+    # pids=()
+    commands=()
+
     mkdir -p $vol
-    # delete files in $vol
+
     rm -rf $vol/testfile*
-    # delete old log files
     rm -rf ./log/log*
 
-    # test io performance in parallel
-    pids=()
+    PIPE=my_sync_pipe
+    mkfifo $PIPE
+    
     local i=1
-    for (( i=1; i<=$parallel_count; i++ ))
+    for (( i=1; i<=$separately_count; i++ ))
     do
-        rm -rf $vol/testfile$i
-        dd if=/dev/zero of=$vol/testfile$i bs=$bs count=$copy_count  2>> ./log/log$i &
-        pids+=($!)
+        (
+            read -r < $PIPE
+            dd if=/dev/zero of=$vol/testfile$i bs=$bs count=1  2>> ./log/log$i
+        ) &
     done
 
-    # delete files in $vol
+    for i in $(seq 1 $NUM_PROCESSES); do
+        echo "Go" > $PIPE
+    done
+
+    wait
+    rm $PIPE
+    
     rm -rf $vol/testfile*
-
-    # waiting for pids
-    for pid in ${pids[@]}
-    do
-        wait $pid
-    done
     return 0
 }
 
@@ -48,65 +51,65 @@ calculate_io_bandwidth_average(){
     sum_bandwith=0
     for line in ${report[@]}
     do
-        value=`echo $line | awk -F":" '{print $1}'`
-        unit=`echo $line | awk  -F":" '{print $2}'`
-        bandwithd=$(echo $value ${unit_map[$unit]} | awk '{printf "%4.3f\n",$1*$2}')
-        sum_bandwith="$( bc <<<"$sum_bandwith + $bandwithd" )"
+    value=`echo $line | awk -F":" '{print $1}'`
+    unit=`echo $line | awk  -F":" '{print $2}'`
+    
+    bandwithd=$(echo $value ${unit_map[$unit]} | awk '{printf "%4.3f\n",$1*$2}')
+    sum_bandwith="$( bc <<<"$sum_bandwith + $bandwithd" )"
     done
 
-    avg_bandwith=$(echo $sum_bandwith $length | awk '{printf "%4.3f\n",$1/$2}')
-    avg_mb_bandwith=$(echo $avg_bandwith ${unit_map["MB/s"]} | awk '{printf "%4.3f\n",$1/$2}')
+    avg_bandwidth=$(echo $sum_bandwith $length | awk '{printf "%4.3f\n",$1/$2}')
+    sum_mb_bandwidth=$(echo $sum_bandwith ${unit_map["MB/s"]} | awk '{printf "%4.3f\n",$1/$2}')
+    avg_mb_bandwidth=$(echo $avg_bandwidth ${unit_map["MB/s"]} | awk '{printf "%4.3f\n",$1/$2}')
 
-    echo "($avg_bandwith, $avg_mb_bandwith)"
+    echo "$avg_bandwidth $avg_mb_bandwidth $sum_mb_bandwidth"
 }
 
 echo_result(){
     local vol=$1
     local bs=$2
-    local parallel_count=$3
-    local copy_count=$4
-    local avg_bandwith=$5
-    local avg_mb_bandwith=$6
-    echo "vol=$vol, bs=$bs, parallel_count=$parallel_count, copy_count=$copy_count, $avg_bandwith Bytes/s,  $avg_mb_bandwith MB/s"
+    local separately_count=$3
+    local avg_bandwidth=$4
+    local avg_mb_bandwidth=$5
+    local sum_mb_bandwidth=$6
+    echo "vol=$vol, bs=$bs, separately_count=$separately_count, $avg_bandwidth Bytes/s, $avg_mb_bandwidth MB/s, $sum_mb_bandwidth MB/s"
 }
 
 
 test_single_bs(){
     local vol=$1
     local bs=$2
-    local parallel_count=$3
-    local copy_count=$4
+    local separately_count=$3
 
-    test_io $vol $bs $parallel_count $copy_count
-
+    test_io $vol $bs $separately_count
     local bw_list=`calculate_io_bandwidth_average`
-    local avg_bandwith=${bw_list[0]}
-    local avg_mb_bandwith=${bw_list[1]}
 
-    echo_result $vol $bs $parallel_count $copy_count $avg_bandwith $avg_mb_bandwith
+    local avg_bandwidth=`echo $bw_list | awk -F" " '{print $1}'`
+    local avg_mb_bandwidth=`echo $bw_list | awk  -F" " '{print $2}'`
+    local sum_mb_bandwidth=`echo $bw_list | awk  -F" " '{print $3}'`
+
+    echo_result $vol $bs $separately_count $avg_bandwidth $avg_mb_bandwidth $sum_mb_bandwidth
 }
 
 
 test_multiple_bs(){
     local vol=$1
-    local parallel_count=$2
+    local separately_count=$2
+    local i=1
     
-    # test 1kBytes ~ 1MBytes
-    local bs_k=1
-    while [ $bs_k -le 1024 ]
-    do 
-        test_single_bs $vol ${bs_k}k $parallel_count 1 
-        bs_k=$(($bs_k*2)) 
-    done 
-    # test 1MBytes ~ 512MBytes
-    local bs_M=1
-    while [ $bs_M -le 512 ]
-    do 
-        test_single_bs $vol ${bs_M}M $parallel_count 1 
-        bs_M=$((bs_M*2))
-    done 
+     i=1; while [ $i -le 512 ]
+        do 
+            test_single_bs $vol ${i}k $separately_count
+            i=$(($i*2)) 
+        done 
+     i=1; while [ $i -le 512 ]
+         do 
+             test_single_bs $vol ${i}M $separately_count
+             i=$((i*2))
+         done 
 }
 
+
 vol=$1
-parallel_count=$2
-test_multiple_bs $vol $parallel_count
+separately_count=$2
+test_multiple_bs $vol $separately_count
---
+++ readme.md
@@ -0,0 +1,51 @@
+# Glusterfs 환경에서 병렬 파일 입출력 성능 분석
+
+다양한 glusterfs 조합에서 파일 I/O 성능을 실험하기 위해 시작한 프로젝트 입니다. 자세한 설명은 아래 링크를 참고하시기 바랍니다.
+
+[NFS에서 분산 파일 시스템 Glusterfs로 전환](https://deagwon.com/post/60)
+
+
+## 실험 목적
+다양한 glusterfs 볼륨별 다중 및 단일 파일 I/O 성능 분석
+
+## 실험 환경
+- Nodes: 8EA
+- OS: Centos7
+- CPU: Intel Xeon Gold 6240R 2.4GHz 48 cores
+- Network Switch
+    - QFX5210 SWITCH, jupyter
+    - bandwidht: 40GBps
+- HDD
+    - RAID Controller: MegaRAID SAS-3 3108(64 bits width, 66MHz clock, 528 MB/s)
+    - disk: INSPUR, AVAGO, 25TiB (27TB), ansiversion=5 logicalsectorsize=512 mount.fstype=xfs mount.options=rw,relatime,attr2,inode64,sunit=512,swidth=512,noquota sectorsize=4096 **3EA**
+
+## 실험 구성
+- Distributed 8
+- Disperse 8 (Redundancy 2)
+- Distributed 4 Replicated 2
+- Distributed 2 Disperse 4 (Redundancy 1)
+
+## 실험 결과
+
+|              | nfs            | distribute 8   | dispersed 8, redundancy 2 | distributed 2, dispersed 4, redundancy 1 | distributed 4, replicated 2 |
+| ------------ | -------------- | -------------- | ------------------------- | ---------------------------------------- | --------------------------- |
+| brick 수      | 1 (146 TB)     | 8 (204 TB)     | 8 (204 TB)                | 8 (204 TB)                               | 8 (204 TB)                  |
+| 스토리지 용량      | 146 TB         | 204 TB         | 154 TB                    | 153 TB                                   | 102TB                       |
+| 단일 파일        | 40 ~ 300 MBps  | 310 ~ 320 MB/s | 290 MB/s                  | 300 MB/s                                 | 290 MB/s                    |
+| 다중 파일(8개 동시) | 300 ~ 600 MBps | 2.1 ~ 2.3 GB/s | 1.2 ~ 1.6 GB/s            | 1.5 ~ 1.8 GB/s                           | 1.1 ~ 1.7 GB/s              |
+
+### 단일 파일 I/O
+<img src="images/single-file.png"/>
+
+### 다중 파일 I/O
+<img src="images/multiple-file.png"/>
+
+
+## reference
+- https://docs.gluster.org/en/latest/
+- https://access.redhat.com/documentation/en-us/red_hat_gluster_storage/3.1/html/administration_guide/chap-recommended-configuration_dispersed
+- https://access.redhat.com/documentation/en-us/red_hat_gluster_storage/3.3/html/administration_guide/chap-red_hat_storage_volumes-creating_dispersed_volumes_1
+- https://portal.nutanix.com/page/documents/kbs/details?targetId=kA07V0000004U9TSAU
+- https://en.wikipedia.org/wiki/Named_pipe
+- https://linuxhint.com/send-process-background-linux/
+- https://man7.org/linux/man-pages/man1/dd.1.html
